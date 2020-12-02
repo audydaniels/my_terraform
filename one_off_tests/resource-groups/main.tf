8ca6c99740e06e48adb3cf7a9ca5e762414a0d2e
Ajd Virtual box <audy.daniels@gmail.com>
2020-12-02 19:41:18 +0000

    separated JSON

--- one_off_tests/resource-groups/main.tf
+++ one_off_tests/resource-groups/main.tf
@@ -13,35 +13,25 @@ variable "app_name" {
 
 
 
-resource "aws_instance" "hosta" {
-  ami 	= "ami-0b0f4c27376f8aa79"
-  instance_type = "t2.micro"
+#resource "aws_instance" "hosta" {
+#  ami 	= "ami-0b0f4c27376f8aa79"
+#  instance_type = "t2.micro"
 
-  tags = {
-    key	   = "Name"
-    Name   = "aa-2"
-    enviornment	= "prod"
-    Application = "Mongo"
-  } 
-}
+#  tags = {
+#    key	   = "Name"
+#    Name   = "aa-2"
+#    enviornment	= "prod"
+#    Application = "Mongo"
+#  } 
+#}
 
 
 resource "aws_resourcegroups_group" "test" {
   name = "test-group"
 
   resource_query {
-    query = <<JSON
-{
-  "ResourceTypeFilters": [
-    "AWS::EC2::Instance"
-  ],
-  "TagFilters": [
-    {
-      "key": "enviornment",
-      "Values": ["prod"]
-    }
-  ]
-}
-JSON
+    query = file("query.json")
+    
   }
 }
+
