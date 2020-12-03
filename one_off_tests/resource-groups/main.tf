444e0af37fff56349799f4a3fc7b1c9c9aa1aa13
audydaniels <audy.daniels@gmail.com>
2020-12-03 06:07:07 -0600

    starting search and cleanup

--- one_off_tests/resource-groups/main.tf
+++ one_off_tests/resource-groups/main.tf
@@ -13,21 +13,21 @@ provider "aws" {
 
 
 
-#resource "aws_instance" "hosta" {
-#  ami 	= "ami-0b0f4c27376f8aa79"
-#  instance_type = "t2.micro"
+resource "aws_instance" "hosta" {
+  ami 	= "ami-0b0f4c27376f8aa79"
+  instance_type = "t2.micro"
 
-#  tags = {
-#    key	   = "Name"
-#    Name   = "aa-2"
-#    enviornment	= "prod"
-#    Application = "Mongo"
-#  } 
-#}
+  tags = {
+    key	   = "Name"
+    Name   = "aa-2"
+    enviornment	= "prod"
+    Application = "Mongo"
+  } 
+}
 
 
-resource "aws_resourcegroups_group" "test" {
-  name = "test-group"
+resource "aws_resourcegroups_group" "prod-mongo" {
+  name = "prod-mango"
 
   resource_query {
     query = file("query3.json")
