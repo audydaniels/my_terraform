c9024bd85bfbe8ff34c2dc391d6c07244aa0ee97
audydaniels <audy.daniels@gmail.com>
2020-12-17 07:19:33 -0600

    add user-data.sh file

---
+++ stage/services/webserver-cluster/user-data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Hello, World" >> index.html
+echo "${data.terraform_remote_state.db.address}" >> index.html
+echo "${data.terraform_remote_state.db.port}" >> index.html
+nohup busybox httpd -f -p "${var.server_port}" &
+
