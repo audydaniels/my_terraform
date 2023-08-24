005b6345fdb39d576a00acccaffd94970a63dfbd
ajd <audy.daniels@gmail.com>
2023-08-24 12:57:20 -0500

    lab work

--- up_and_running_work/instance.tf
+++ up_and_running_work/instance.tf
@@ -3,6 +3,13 @@
 resource "aws_instance" "test" {
     ami = "ami-0430580de6244e02e"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+    
+    user_data = <<-EOF
+        #!/bin/bash
+        echo "Hello, World" > index.html
+        nohup busybox httpd -f -p 8080 &
+        EOF
     
     tags = {
         Name = "terraform-example"
